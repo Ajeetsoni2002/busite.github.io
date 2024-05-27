b3dcfdf2b0c48602b153ed95decd9ed9a7fc63c1
Ajeet Kumar Soni <94032343+Ajeetsoni2002@users.noreply.github.com>
2024-05-28 02:40:19 +0530

    Delete other pages 1/branch page directory

--- other pages 1/branch page/other pages2/papers page/ja22.js
+++
@@ -1,6 +0,0 @@
-const cards = document.querySelectorAll('.card');
-
-cards.forEach((card, index) => {
-    card.style.animationDelay = index * 0.2 + 's'; 
-});
-// Check local storage for user preference
